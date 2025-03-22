9edfe99f8572a66aa688c070f01ae7d082757263
kozie1 <maquuu280@gmail.com>
2025-03-22 16:25:21 +0100

    Usunięcie zdublowanego katalogu calendar_utils_project z repozytorium

--- Desktop/calendar_utils_project/tests/test_date_tools.py
+++
@@ -1,9 +0,0 @@
-import unittest
-from calendar_utils.date_tools import get_day_of_week, add_workdays
-
-class TestDateTools(unittest.TestCase):
-    def test_get_day_of_week(self):
-        self.assertEqual(get_day_of_week("2025-03-22"), "Saturday")
-
-    def test_add_workdays(self):
-        self.assertEqual(add_workdays("2025-03-21", 1), "2025-03-24")
